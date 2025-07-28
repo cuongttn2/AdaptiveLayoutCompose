c0e2a2477d302b7218ec44fe7adc5323118a4cc1
_v <caocuongttn2@gmail.com>
2025-07-28 09:42:19 +0700

    demo supporting-pane layout

--- app/src/main/java/com/example/adaptivelayoutcompose/MainActivity.kt
+++ app/src/main/java/com/example/adaptivelayoutcompose/MainActivity.kt
@@ -7,12 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.adaptivelayoutcompose.ui.supporting_pane.SupportingPaneSample
 import com.example.adaptivelayoutcompose.ui.theme.AdaptiveLayoutComposeTheme
 
+@OptIn(ExperimentalMaterial3AdaptiveApi::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,28 +20,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             AdaptiveLayoutComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    SupportingPaneSample(modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AdaptiveLayoutComposeTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/adaptivelayoutcompose/ui/supporting_pane/SupportingSample.kt
@@ -0,0 +1,145 @@
+package com.example.adaptivelayoutcompose.ui.supporting_pane
+
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.LocalMinimumInteractiveComponentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDragHandle
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.layout.AnimatedPane
+import androidx.compose.material3.adaptive.layout.SupportingPaneScaffold
+import androidx.compose.material3.adaptive.layout.SupportingPaneScaffoldRole
+import androidx.compose.material3.adaptive.layout.rememberPaneExpansionState
+import androidx.compose.material3.adaptive.navigation.rememberSupportingPaneScaffoldNavigator
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.adaptivelayoutcompose.R
+import kotlinx.coroutines.launch
+
+// Create some simple sample data
+private val data = mapOf(
+    "android" to listOf("kotlin", "java", "flutter"),
+    "kotlin" to listOf("backend", "android", "desktop"),
+    "desktop" to listOf("kotlin", "java", "flutter"),
+    "backend" to listOf("kotlin", "java"),
+    "java" to listOf("backend", "android", "desktop"),
+    "flutter" to listOf("android", "desktop")
+)
+
+@ExperimentalMaterial3AdaptiveApi
+@Composable
+fun SupportingPaneSample(modifier: Modifier = Modifier) {
+    var selectedTopic: String by rememberSaveable { mutableStateOf(data.keys.first()) }
+    val navigator = rememberSupportingPaneScaffoldNavigator()
+    val scope = rememberCoroutineScope()
+
+    BackHandler(enabled = navigator.canNavigateBack()) {
+        scope.launch {
+            navigator.navigateBack()
+        }
+    }
+
+    SupportingPaneScaffold(
+        modifier = modifier,
+        directive = navigator.scaffoldDirective,
+        value = navigator.scaffoldValue,
+        supportingPane = {
+            AnimatedPane(
+                modifier = Modifier.padding(all = 16.dp)
+            ) {
+                Column {
+                    Text(
+                        stringResource(R.string.related_content_label),
+                        modifier = Modifier.padding(vertical = 16.dp),
+                        style = MaterialTheme.typography.titleLarge
+                    )
+
+                    LazyColumn {
+                        items(
+                            data.getValue(selectedTopic),
+                            key = { it }
+                        ) { relatedTopic ->
+                            Box(
+                                Modifier
+                                    .fillMaxWidth()
+                                    .padding(all = 4.dp)
+                                    .clickable {
+                                        selectedTopic = relatedTopic
+                                        if (navigator.canNavigateBack()) {
+                                            scope.launch {
+                                                navigator.navigateBack()
+                                            }
+                                        }
+                                    }
+                            ) {
+                                Text(
+                                    text = relatedTopic,
+                                    modifier = Modifier
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }, mainPane = {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
+            )
+            {
+                Text(
+                    stringResource(R.string.main_content_label),
+                    style = MaterialTheme.typography.titleLarge
+                )
+
+                Box(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(all = 8.dp)
+                        .clickable {
+                            scope.launch {
+                                navigator.navigateTo(SupportingPaneScaffoldRole.Supporting)
+                            }
+                        },
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = selectedTopic,
+                        modifier = Modifier
+                            .padding(16.dp)
+                    )
+                }
+            }
+        },
+        paneExpansionState = rememberPaneExpansionState(navigator.scaffoldValue),
+        paneExpansionDragHandle = { state ->
+            val interactionSource =
+                remember { MutableInteractionSource() }
+            VerticalDragHandle(
+                modifier =
+                    Modifier.paneExpansionDraggable(
+                        state,
+                        LocalMinimumInteractiveComponentSize.current,
+                        interactionSource
+                    ), interactionSource = interactionSource
+            )
+        })
+}
