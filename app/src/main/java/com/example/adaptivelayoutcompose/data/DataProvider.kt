3c412b117978a217291b24718615294e427edc39
_v <caocuongttn2@gmail.com>
2025-07-30 11:26:38 +0700

    update image link

--- app/src/main/java/com/example/adaptivelayoutcompose/data/DataProvider.kt
+++ app/src/main/java/com/example/adaptivelayoutcompose/data/DataProvider.kt
@@ -6,253 +6,253 @@ object DataProvider {
     val sweets = listOf(
         Sweets(
             id = 0,
-            imageUrl = "https://source.unsplash.com/V4MBq8kue3U",
+            imageUrl = "https://unsplash.com/photos/a-cupcake-with-white-frosting-and-purple-flowers-pF0_nPICOzQ",
             description = R.string.lorem_ipsum,
             category = Category.Pastry
         ),
         Sweets(
             id = 1,
-            imageUrl = "https://source.unsplash.com/cSzyY2UaFSI",
+            imageUrl = "https://unsplash.com/photos/a-piece-of-cake-on-a-plate-with-a-fork--R7mf6lQ4P8",
             description = R.string.lorem_ipsum,
             category = Category.Misc
         ),
         Sweets(
             id = 2,
-            imageUrl = "https://source.unsplash.com/mGP8gyGb8zY",
+            imageUrl = "https://unsplash.com/photos/cupcakes-on-brown-wooden-round-tray-aJ9XZTzY59c",
             description = R.string.lorem_ipsum,
             category = Category.Candy
         ),
         Sweets(
             id = 3,
-            imageUrl = "https://source.unsplash.com/PL5FZkW0Qkk",
+            imageUrl = "https://images.unsplash.com/photo-1496533141630-d6731a403292?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c3dlZXQlMjBkZXNzZXJ0c3xlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Candy
         ),
         Sweets(
             id = 4,
-            imageUrl = "https://source.unsplash.com/xLvIcAYuuMQ",
+            imageUrl = "https://plus.unsplash.com/premium_photo-1661660005860-f9b172efbb10?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c3dlZXQlMjBkZXNzZXJ0c3xlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Candy
         ),
         Sweets(
             id = 5,
-            imageUrl = "https://source.unsplash.com/PMOoaWCqX_Q",
+            imageUrl = "https://plus.unsplash.com/premium_photo-1700830647950-6bba9ca2a957?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHN3ZWV0JTIwZGVzc2VydHN8ZW58MHx8MHx8fDA%3D",
             description = R.string.lorem_ipsum,
             category = Category.Chocolate
         ),
         Sweets(
             id = 6,
-            imageUrl = "https://source.unsplash.com/yCOzRIbL08E",
+            imageUrl = "https://unsplash.com/photos/a-plate-of-food-qX4UTnpPa6U",
             description = R.string.lorem_ipsum,
             category = Category.Misc
         ),
         Sweets(
             id = 7,
-            imageUrl = "https://source.unsplash.com/ZYKCgsRz9Mg",
+            imageUrl = "https://plus.unsplash.com/premium_photo-1700830647950-6bba9ca2a957?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHN3ZWV0JTIwZGVzc2VydHN8ZW58MHx8MHx8fDA%3D",
             description = R.string.lorem_ipsum,
             category = Category.Pastry
         ),
         Sweets(
             id = 8,
-            imageUrl = "https://source.unsplash.com/Fq54FqucgCE",
+            imageUrl = "https://unsplash.com/photos/a-plate-of-food-qX4UTnpPa6U",
             description = R.string.lorem_ipsum,
             category = Category.Pastry
         ),
         Sweets(
             id = 9,
-            imageUrl = "https://source.unsplash.com/WqCRDVs7ZI8",
+            imageUrl = "https://plus.unsplash.com/premium_photo-1667031518595-9cb4b0d504ef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8c3dlZXQlMjBmb29kfGVufDB8fDB8fHww",
             description = R.string.lorem_ipsum,
             category = Category.Misc
         ),
         Sweets(
             id = 10,
-            imageUrl = "https://source.unsplash.com/gP1YecpRyD8",
+            imageUrl = "https://plus.unsplash.com/premium_photo-1677591560099-176391189f12?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c3dlZXQlMjBmb29kfGVufDB8fDB8fHww",
             description = R.string.lorem_ipsum,
             category = Category.Chocolate
         ),
         Sweets(
             id = 11,
-            imageUrl = "https://source.unsplash.com/hLOLcUwR0Y4",
+            imageUrl = "https://plus.unsplash.com/premium_photo-1714669899928-eb0b28430295?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Pastry
         ),
         Sweets(
             id = 12,
-            imageUrl = "https://source.unsplash.com/mtut50xOeC4",
+            imageUrl = "https://images.unsplash.com/photo-1567877849899-d5e88b882078?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Pastry
         ),
         Sweets(
             id = 13,
-            imageUrl = "https://source.unsplash.com/qZ5lPCPvdXE",
+            imageUrl = "https://images.unsplash.com/photo-1656102728797-d635d8237af2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Pastry
         ),
         Sweets(
             id = 14,
-            imageUrl = "https://source.unsplash.com/uG3Vu5TXKxE",
+            imageUrl = "https://images.unsplash.com/photo-1608756705169-8908b864de49?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Pastry
         ),
         Sweets(
             id = 15,
-            imageUrl = "https://source.unsplash.com/90HdOlGbjck",
+            imageUrl = "https://media.istockphoto.com/id/1719204402/photo/unhealthy-eating-concept-close-up-of-a-woman-eating-donut-and-drinking-cola.webp?a=1&b=1&s=612x612&w=0&k=20&c=Djqvj9AzLs9GwlAreLhPDxgnl7aFodhICPtVupfIpNI=",
             description = R.string.lorem_ipsum,
             category = Category.Pastry
         ),
         Sweets(
             id = 16,
-            imageUrl = "https://source.unsplash.com/BhK9JdaBTvk",
+            imageUrl = "https://media.istockphoto.com/id/1490797933/photo/tasty-colorful-jelly-candies-as-background-above-view.webp?a=1&b=1&s=612x612&w=0&k=20&c=MJA-N4oU1Y_ynMjU_3owpDp907VZg33WmHFtEPOMTCs=",
             description = R.string.lorem_ipsum,
             category = Category.Pastry
         ),
         Sweets(
             id = 17,
-            imageUrl = "https://source.unsplash.com/w0_w3N_hG00",
+            imageUrl = "https://media.istockphoto.com/id/2152882524/photo/multicolored-ice-cream-cones-and-fruits-shot-from-above-on-gray-background.webp?a=1&b=1&s=612x612&w=0&k=20&c=7qdnYss-LCyOqz3lISzShqalpP4j6x46LnM4yH8VK1w=",
             description = R.string.lorem_ipsum,
             category = Category.Pastry
         ),
         Sweets(
             id = 18,
-            imageUrl = "https://source.unsplash.com/AguGBqWbmME",
+            imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Pastry
         ),
         Sweets(
             id = 19,
-            imageUrl = "https://source.unsplash.com/yE_jI4KApfc",
+            imageUrl = "https://media.istockphoto.com/id/2152882524/photo/multicolored-ice-cream-cones-and-fruits-shot-from-above-on-gray-background.webp?a=1&b=1&s=612x612&w=0&k=20&c=7qdnYss-LCyOqz3lISzShqalpP4j6x46LnM4yH8VK1w=",
             description = R.string.lorem_ipsum,
             category = Category.Chocolate
         ),
         Sweets(
             id = 20,
-            imageUrl = "https://source.unsplash.com/p6OLZPnq810",
+            imageUrl = "https://media.istockphoto.com/id/1490797933/photo/tasty-colorful-jelly-candies-as-background-above-view.webp?a=1&b=1&s=612x612&w=0&k=20&c=MJA-N4oU1Y_ynMjU_3owpDp907VZg33WmHFtEPOMTCs=",
             description = R.string.lorem_ipsum,
             category = Category.Pastry
         ),
         Sweets(
             id = 21,
-            imageUrl = "https://source.unsplash.com/AHF_ZktTL6Q",
+            imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Candy
         ),
         Sweets(
             id = 22,
-            imageUrl = "https://source.unsplash.com//LU_fCezP9-o",
+            imageUrl = "https://media.istockphoto.com/id/1490797933/photo/tasty-colorful-jelly-candies-as-background-above-view.webp?a=1&b=1&s=612x612&w=0&k=20&c=MJA-N4oU1Y_ynMjU_3owpDp907VZg33WmHFtEPOMTCs=",
             description = R.string.lorem_ipsum,
             category = Category.Candy
         ),
         Sweets(
             id = 23,
-            imageUrl = "https://source.unsplash.com/_C5zsV_p-YI",
+            imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Candy
         ),
         Sweets(
             id = 24,
-            imageUrl = "https://source.unsplash.com/aXq1oCCjlVM",
+            imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Candy
         ),
         Sweets(
             id = 25,
-            imageUrl = "https://source.unsplash.com/cRwZACu3kQI",
+            imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Pastry
         ),
         Sweets(
             id = 26,
-            imageUrl = "https://source.unsplash.com/8XkNFQG_cgk",
+            imageUrl = "https://media.istockphoto.com/id/1490797933/photo/tasty-colorful-jelly-candies-as-background-above-view.webp?a=1&b=1&s=612x612&w=0&k=20&c=MJA-N4oU1Y_ynMjU_3owpDp907VZg33WmHFtEPOMTCs=",
             description = R.string.lorem_ipsum,
             category = Category.Candy
         ),
         Sweets(
             id = 27,
-            imageUrl = "https://source.unsplash.com/FDYbS43jUrU",
+            imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Pastry
         ),
         Sweets(
             id = 28,
-            imageUrl = "https://source.unsplash.com/-ayOfwsd9mY",
+            imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Candy
         ),
         Sweets(
             id = 29,
-            imageUrl = "https://source.unsplash.com/dcPNZeSY3yk",
+            imageUrl = "https://media.istockphoto.com/id/1490797933/photo/tasty-colorful-jelly-candies-as-background-above-view.webp?a=1&b=1&s=612x612&w=0&k=20&c=MJA-N4oU1Y_ynMjU_3owpDp907VZg33WmHFtEPOMTCs=",
             description = R.string.lorem_ipsum,
             category = Category.Chocolate
         ),
         Sweets(
             id = 30,
-            imageUrl = "https://source.unsplash.com/tWe8ib-cnXY",
+            imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Chocolate
         ),
         Sweets(
             id = 31,
-            imageUrl = "https://source.unsplash.com/r-hQw_obFd0",
+            imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Chocolate
         ),
         Sweets(
             id = 32,
-            imageUrl = "https://source.unsplash.com/EwaJbJvS9io",
+            imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Chocolate
         ),
         Sweets(
             id = 33,
-            imageUrl = "https://source.unsplash.com/LjzAqkZnGFM",
+            imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Chocolate
         ),
         Sweets(
             id = 34,
-            imageUrl = "https://source.unsplash.com/PqYvDBwpXpU",
+            imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Pastry
         ),
         Sweets(
             id = 35,
-            imageUrl = "https://source.unsplash.com/89h9zKa0L0g",
+            imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Pastry
         ),
         Sweets(
             id = 36,
-            imageUrl = "https://source.unsplash.com/OAC2cpzNCxs",
+           imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Pastry
         ),
         Sweets(
             id = 37,
-            imageUrl = "https://source.unsplash.com/PGQxoFvt14o",
+            imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Misc
         ),
         Sweets(
             id = 38,
-            imageUrl = "https://source.unsplash.com/cLpdEA23Z44",
+            imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Misc
         ),
         Sweets(
             id = 39,
-            imageUrl = "https://source.unsplash.com/ewOrvEa87j4",
+          imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Misc
         ),
         Sweets(
             id = 40,
-            imageUrl = "https://source.unsplash.com/uy9DJw9e_vs",
+          imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Misc
         ),
         Sweets(
             id = 41,
-            imageUrl = "https://source.unsplash.com/dIs-MqalSSE",
+            imageUrl = "https://images.unsplash.com/photo-1551692272-4c55f60d2e17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN3ZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
             description = R.string.lorem_ipsum,
             category = Category.Misc
         )
